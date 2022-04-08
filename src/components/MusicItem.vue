9f572beed2022a6a1bb1543b5264a7714d320892
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-04-08 17:09:14 +0200

    creazione componente che mi servirà per visualizzare il singolo album della galleria

---
+++ src/components/MusicItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      
+  </div>
+</template>
+
+<script>
+/* modello dati del singolo oggetto di cui è composto l'array di oggetti che ottengo in risposta alla chiamata api
+author: "Bon Jovi"
+genre: "Rock"
+poster: "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg"
+title: "New Jersey"
+year: "1988"*/
+export default {
+    name:'MusicItem',
+    props:{
+        singleAlbum:Object
+    }
+}
+</script>
+
+<style>
+
+</style>
+
+
+
+
+
--- src/components/MusicList.vue
+++ src/components/MusicList.vue
@@ -1,7 +1,14 @@
 <template>
   <main>
       <div class="container">
-
+          <div v-if="albums.length > 0">    <!--sino a che il server non risponde restituendomi l'array di 10 elementi (vedi il console log) visualizzo il componente con il loader-->
+            <MusicItem 
+                v-for="(item,index) in albums"
+                :key="index"
+                :singleAlbum="item"
+            >
+          </div>
+          <div v-else><!--qui andrà il componente con il loader cosicchè mentre l'utente attende il risultato del server e quindi la visualizzazione degli elementi nella pagina verrà visualizzato un loader---></div>
       </div>
   </main>
 </template>
@@ -16,15 +23,25 @@ export default {
     props:{
         url:String
     },
-    mounted(){
+    mounted(){  // mounted() è la prima funzione che si avvia; mounted() lancia la funzione per far avviare il processo per prendere i dati dal server e passarli nei data del componente
         this.loadData();
     },
     methods:{
-        loadData(){
+        loadData(){ //funzione per avviare la chiamata dell'api così da prendere gli elementi che mi servono e passarli ai data (cioè l'array albums)
             axios.get(this.url,true).then((response)=>{
-                console.log(response);
+                //console.log(response);
+                const resultFromApi = response;
+                console.log(resultFromApi); //stampa per vedere che tipo di struttura dati ottengo in risposta alla chiamata api 
+                if(resultFromApi.status === 200) //check sullo status del server
+                {
+                    this.albums = resultFromApi.data.response;
+                    console.log(this.albums[0]);
+                }
             })
         }
+    },
+    components:{
+        MusicItem
     }
 }
 </script>
