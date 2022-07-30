6c2f3f27d2212a6cd347180872e42dac5abe0d99
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-07-30 17:49:32 +0200

    posta select dentro un div nel MusicSearch.vue così da non farla espandere

--- src/components/MusicSearch.vue
+++ src/components/MusicSearch.vue
@@ -7,10 +7,12 @@
       <option value="metal">Metal</option>
   </select-->
   <!--input type="text" placeholder="Cerca" v-model="inputText" @keyup.enter="$emit('search',inputText)"-->
-  <select v-model="selectedCategory" @change="changeCategory">
-      <option value="">All categories</option>
-      <option v-for="category in categories" :key="category" :value="category">{{category}}</option>
-  </select>
+  <div>
+    <select v-model="selectedCategory" @change="changeCategory">
+        <option value="">All categories</option>
+        <option v-for="category in categories" :key="category" :value="category">{{category}}</option>
+    </select>
+  </div>
 </template>
 
 <script>
